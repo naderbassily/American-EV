f8eba9777c989680e84e040da9d4cfaf6fdc11cd
Nader Bassily <me@naderramsis.com>
2026-09-14 12:59:00 -0400

    Add American EV theme thumbnail

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'AEV_THEME_VERSION', '1.3.2' );
+define( 'AEV_THEME_VERSION', '1.3.3' );
 define( 'AEV_NOTIFICATION_EMAIL', 'info@americanevsolutions.com' );
 define( 'AEV_FROM_EMAIL', 'info@americanevsolutions.com' );
 
